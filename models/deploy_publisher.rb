bed641bdf165fbe81c34fa14a7e7188d26f47636
Sunggun Yu <sunggun.yu@gmail.com>
2014-01-05 02:20:02 -0500

    Refactoring and adding unzip logic for Capitomcat recipe archive file : Still working on refactoring

--- models/deploy_publisher.rb
+++ models/deploy_publisher.rb
@@ -1,29 +1,38 @@
 require 'fileutils'
 require 'erb'
+require 'zip'
 
 class DeployPublisher < Jenkins::Tasks::Publisher
 
-  attr_accessor :hosts
-  attr_accessor :isParallel
-  attr_accessor :user, :password, :tomcat_user, :tomcat_user_group, :tomcat_cmd_user
-  attr_accessor :local_war_file, :tomcat_port, :remote_docBase, :remote_tomcat_cmd, :remote_tomcat_work_dir
-  attr_accessor :context_name, :remote_context_file
+  attr_accessor :remote_hosts,
+                :remote_tomcat_home,
+                :remote_tomcat_hostname,
+                :remote_tomcat_cmd,
+                :tomcat_user,
+                :tomcat_user_group,
+                :tomcat_port
+  attr_accessor :local_war_file,
+                :is_parallel,
+                :remote_doc_base
+  attr_accessor :update_context, :context_name
 
-  display_name "Deploy via Capitomcat"
+  display_name 'Deploy via Capitomcat'
   # Invoked with the form parameters when this extension point
   # is created from a configuration screen.
   def initialize(attrs = {})
-    @remote_hosts = attrs["remote_hosts"]
-    @isParallel = attrs["isParallel"]
-    @tomcat_user = attrs["tomcat_user"]
-    @tomcat_user_group = attrs["tomcat_user_group"]
-    @tomcat_port = attrs["tomcat_port"]
-    @local_war_file = attrs["local_war_file"]
-    @remote_docBase = attrs["remote_docBase"]
-    @remote_tomcat_cmd = attrs["remote_tomcat_cmd"]
-    @remote_tomcat_work_dir = attrs["remote_tomcat_work_dir"]
-    @context_name = attrs["context_name"]
-    @remote_context_file = attrs["remote_context_file"]
+    @remote_hosts = attrs['remote_hosts']
+    @remote_tomcat_home = attrs['remote_tomcat_home']
+    @remote_tomcat_hostname = attrs['remote_tomcat_hostname']
+    @remote_tomcat_cmd = attrs['remote_tomcat_cmd']
+    @tomcat_user = attrs['tomcat_user']
+    @tomcat_user_group = attrs['tomcat_user_group']
+    @tomcat_port = attrs['tomcat_port']
+
+    @local_war_file = attrs['local_war_file']
+    @is_parallel = attrs['is_parallel']
+    @remote_docBase = attrs['remote_doc_base']
+    @update_context = attrs['update_context']
+    @context_name = attrs['context_name']
   end
 
   ##
@@ -32,7 +41,20 @@ class DeployPublisher < Jenkins::Tasks::Publisher
   # @param [Jenkins::Model::Build] build the build which will begin
   # @param [Jenkins::Model::Listener] listener the listener for this build.
   def prebuild(build, listener)
-    # do any setup that needs to be done before this build runs.
+    workspace = build.send(:native).workspace.to_s
+
+    # Copy recipe into workspace
+    install_recipe(workspace)
+
+    if is_parallel
+      puts "#{is_parallel}"
+    end
+
+    puts "#{update_context}"
+    puts "#{context_name}"
+
+    # Generate and save config template
+    #save_config_template(workspace)
   end
 
   ##
@@ -44,37 +66,64 @@ class DeployPublisher < Jenkins::Tasks::Publisher
   def perform(build, launcher, listener)
     # actually perform the build step
 
-    workspace = build.send(:native).workspace.to_s
+    # Execute the Capistrano script
+    #launch_cap(launcher, listener)
+  end
 
+  # Install basic recipe into workspace
+  def install_recipe(workspace)
     # Copy recipe into workspace
-    recipe_dir = File.expand_path("../template/recipe/single", __FILE__)
-    FileUtils.cp_r recipe_dir, Pathname.new(workspace)
+    #recipe_dir = File.expand_path('../template/recipe/single', __FILE__)
+    #FileUtils.cp_r recipe_dir, Pathname.new(workspace)
+    recipe_zip = File.expand_path('../template/recipes/single.zip', __FILE__)
+    unzip_file(recipe_zip, workspace)
+  end
 
-    remote_hosts = "#{@remote_hosts}"
-    isParallel = "#{@isParallel}"
-    tomcat_user = "#{@tomcat_user}"
-    tomcat_user_group = "#{@tomcat_user_group}"
-    tomcat_port = "#{@tomcat_port}"
-    local_war_file = "#{@local_war_file}"
-    remote_docBase = "#{@remote_docBase}"
-    remote_tomcat_cmd = "#{@remote_tomcat_cmd}"
-    remote_tomcat_work_dir = "#{@remote_tomcat_work_dir}"
-    context_name = "#{@context_name}"
-    remote_context_file = "#{@remote_context_file}"
-    isUpdateContext = true
+  # Generate config template
+  def generate_config_template
+    bind_erb_variables()
+    template_file = File.read(File.expand_path('../template/config.rb.erb', __FILE__))
+    return ERB.new(template_file).result(binding)
+  end
 
-    # Create config.rb from template file
-    template_file = File.read(File.expand_path("../template/template/config.rb.erb", __FILE__))
-    context_name = context_name
-    remote_docBase = remote_docBase
-
-    config_dir = Pathname.new( workspace + '/single/config')
+  # Save config template file into workspace
+  def save_config_template (workspace)
+    config_dir = Pathname.new(workspace + '/single/config')
     file = config_dir.join('config.rb')
-
     File.open(file, 'w+') do |f|
-      f.write(ERB.new(template_file).result(binding))
+      f.write(generate_config_template)
     end
-# 
-    launcher.execute("bash", "-c", "cd #{workspace}/single; cap single deploy:startRelease", { :out => listener })
+  end
+
+  # Launch CAP command
+  def launch_cap(launcher, listener)
+    launcher.execute('bash', '-c', 'cd #{workspace}/single; cap single deploy:startRelease', {:out => listener})
+  end
+
+  # Unzip file
+  def unzip_file(file, destination)
+    if File.exist?(destination) then
+      FileUtils.rm_rf destination
+    end
+    Zip::File.open(file) { |zip_file|
+      zip_file.each { |f|
+        f_path=File.join(destination, f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+        zip_file.extract(f, f_path)
+      }
+    }
+  end
+
+  def bind_erb_variables
+    set_remote_context_file()
+    set_remote_tomcat_work_dir()
+  end
+
+  def set_remote_context_file
+
+  end
+
+  def set_remote_tomcat_work_dir
+
   end
 end
