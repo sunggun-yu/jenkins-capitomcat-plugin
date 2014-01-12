24f6817d9c56f41168cafbf49b01e37c397dc00e
Sunggun Yu <sunggun.yu@gmail.com>
2014-01-12 03:13:35 -0500

    RC-1

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem "jenkins-plugin-runtime"
 gem "rubyzip", "~> 1.1.0"
+gem "capistrano", "~> 3.0.1"
+gem 'capitomcat', '~> 1.0.0'
--- models/deploy_publisher.rb
+++ models/deploy_publisher.rb
@@ -1,38 +1,44 @@
 require 'fileutils'
-require 'erb'
-require 'zip'
+require 'rake'
+require 'capistrano/all'
+require 'capistrano/setup'
+require 'capistrano/deploy'
 
 class DeployPublisher < Jenkins::Tasks::Publisher
 
   attr_accessor :remote_hosts,
-                :remote_tomcat_home,
-                :remote_tomcat_hostname,
-                :remote_tomcat_cmd,
                 :tomcat_user,
                 :tomcat_user_group,
-                :tomcat_port
+                :tomcat_port,
+                :tomcat_cmd,
+                :use_tomcat_user_cmd,
+                :tomcat_war_file,
+                :tomcat_context_path,
+                :tomcat_context_file,
+                :tomcat_work_dir
   attr_accessor :local_war_file,
-                :is_parallel,
-                :remote_doc_base
-  attr_accessor :update_context, :context_name
+                :use_parallel,
+                :use_context_update
 
   display_name 'Deploy via Capitomcat'
   # Invoked with the form parameters when this extension point
   # is created from a configuration screen.
   def initialize(attrs = {})
     @remote_hosts = attrs['remote_hosts']
-    @remote_tomcat_home = attrs['remote_tomcat_home']
-    @remote_tomcat_hostname = attrs['remote_tomcat_hostname']
-    @remote_tomcat_cmd = attrs['remote_tomcat_cmd']
     @tomcat_user = attrs['tomcat_user']
     @tomcat_user_group = attrs['tomcat_user_group']
     @tomcat_port = attrs['tomcat_port']
+    @tomcat_cmd = attrs['tomcat_cmd']
+    @use_tomcat_user_cmd = attrs['use_tomcat_user_cmd']
+    @tomcat_war_file = attrs['tomcat_war_file']
+    @tomcat_context_path = attrs['tomcat_context_path']
+    @tomcat_context_file = attrs['tomcat_context_file']
+    @tomcat_work_dir = attrs['tomcat_work_dir']
 
     @local_war_file = attrs['local_war_file']
-    @is_parallel = attrs['is_parallel']
-    @remote_docBase = attrs['remote_doc_base']
-    @update_context = attrs['update_context']
-    @context_name = attrs['context_name']
+
+    @use_parallel = attrs['use_parallel']
+    @use_context_update = attrs['use_context_update']
   end
 
   ##
@@ -41,18 +47,6 @@ class DeployPublisher < Jenkins::Tasks::Publisher
   # @param [Jenkins::Model::Build] build the build which will begin
   # @param [Jenkins::Model::Listener] listener the listener for this build.
   def prebuild(build, listener)
-    workspace = build.send(:native).workspace.to_s
-
-    # Copy recipe into workspace
-    install_recipe(workspace)
-
-    if is_parallel
-      puts "#{is_parallel}"
-    end
-
-    puts "#{update_context}"
-    puts "#{context_name}"
-
     # Generate and save config template
     #save_config_template(workspace)
   end
@@ -68,62 +62,49 @@ class DeployPublisher < Jenkins::Tasks::Publisher
 
     # Execute the Capistrano script
     #launch_cap(launcher, listener)
+    do_capitomcat(launcher, listener)
   end
 
-  # Install basic recipe into workspace
-  def install_recipe(workspace)
-    # Copy recipe into workspace
-    #recipe_dir = File.expand_path('../template/recipe/single', __FILE__)
-    #FileUtils.cp_r recipe_dir, Pathname.new(workspace)
-    recipe_zip = File.expand_path('../template/recipes/single.zip', __FILE__)
-    unzip_file(recipe_zip, workspace)
-  end
+  def do_capitomcat(launcher, listener)
+    set :stage, :dev
 
-  # Generate config template
-  def generate_config_template
-    bind_erb_variables()
-    template_file = File.read(File.expand_path('../template/config.rb.erb', __FILE__))
-    return ERB.new(template_file).result(binding)
-  end
+    set :format, :pretty
+    set :log_level, :info
+    set :pty, true
+    set :use_sudo, true
 
-  # Save config template file into workspace
-  def save_config_template (workspace)
-    config_dir = Pathname.new(workspace + '/single/config')
-    file = config_dir.join('config.rb')
-    File.open(file, 'w+') do |f|
-      f.write(generate_config_template)
+    # Server definition section
+    role :app, @remote_hosts.split(',')
+
+    # Remote Tomcat server setting section
+    set :tomcat_user, @tomcat_user
+    set :tomcat_user_group, @tomcat_user_group
+    set :tomcat_port, @tomcat_port
+    set :tomcat_cmd, @tomcat_cmd
+    set :use_tomcat_user_cmd, @use_tomcat_user_cmd
+    set :tomcat_war_file, @tomcat_war_file
+    set :tomcat_context_path, @tomcat_context_path
+    set :tomcat_context_file, @tomcat_context_file
+    set :tomcat_work_dir, @tomcat_work_dir
+
+    # Deploy setting section
+    set :local_war_file, @local_war_file
+    set :context_template_file, File.expand_path('../templates/context.xml.erb', __FILE__).to_s
+    set :use_parallel, @use_parallel
+    set :use_context_update, @use_context_update
+    set :listener, listener
+
+    begin
+      cap_file = File.expand_path('../tasks/capitomcat.cap', __FILE__).to_s
+      capistrano = Capistrano::Application.new
+      capistrano.add_import(cap_file)
+      capistrano.load_imports
+
+      listener.info 'Starting Capitomcat Tomcat deploy'
+      capistrano.invoke('capitomcat_jenkins:deploy')
+    rescue
+      listener.error 'Capitomcat deploy failed'
     end
-  end
-
-  # Launch CAP command
-  def launch_cap(launcher, listener)
-    launcher.execute('bash', '-c', 'cd #{workspace}/single; cap single deploy:startRelease', {:out => listener})
-  end
-
-  # Unzip file
-  def unzip_file(file, destination)
-    if File.exist?(destination) then
-      FileUtils.rm_rf destination
-    end
-    Zip::File.open(file) { |zip_file|
-      zip_file.each { |f|
-        f_path=File.join(destination, f.name)
-        FileUtils.mkdir_p(File.dirname(f_path))
-        zip_file.extract(f, f_path)
-      }
-    }
-  end
-
-  def bind_erb_variables
-    set_remote_context_file()
-    set_remote_tomcat_work_dir()
-  end
-
-  def set_remote_context_file
-
-  end
-
-  def set_remote_tomcat_work_dir
-
+    listener.info 'Capitomcat deploy has finished successfully'
   end
 end
